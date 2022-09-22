b41d6f196d2c359d5b8a72adbeb0db7ba3597086
crisp <2715554797@qq.com>
2022-09-22 23:10:32 +0800

    add gateway

---
+++ CrispMallGateWay/src/test/java/cn/crisp/crispmallgateway/CrispMallGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.crisp.crispmallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrispMallGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
